ab5200a83b236e3678da1594b29c8fa01cdc62f5
sadaqatmustafa123 <sadaqatmustafa12345@gmail.com>
2023-08-17 02:12:09 +0500

    Added the code of Chat App

---
+++ lib/main.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Chats"),
+        ),
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 34, 112, 176),
+                leading: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromARGB(255, 52, 211, 219),
+                ),
+                title: Text("New User"),
+                textColor: Colors.white,
+                subtitle: Text("New Mesaage"),
+                trailing: Text("09:00 Am"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
